70cd9ea261e5bf5c663bff1ef2e80753d9a709dd
Timur <jungle8888@mail.ru>
2025-03-01 19:08:18 +0300

    add base docusaurus setup

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,77 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'DataRush',
+  tagline: 'Изучите основы анализа данных здесь!',
+  favicon: 'https://prod-team-15-2pc0i3lc.final.prodcontest.ru/logo.svg',
+
+  url: 'https://prod-team-15-2pc0i3lc.final.prodcontest.ru',
+  baseUrl: '/',
+
+  organizationName: 'megazord',
+  projectName: 'megazord',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {},
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    image: 'https://prod-team-15-2pc0i3lc.final.prodcontest.ru/logo.svg',
+    navbar: {
+      title: 'DataRush',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'https://prod-team-15-2pc0i3lc.final.prodcontest.ru/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'defaultSidebar',
+          position: 'left',
+          label: 'Документация',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Документация',
+          items: [
+            {
+              label: 'Начало',
+              to: '/docs/intro',
+            },
+          ],
+        },
+      ],
+      copyright: `Создано для Megazord ♥`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
