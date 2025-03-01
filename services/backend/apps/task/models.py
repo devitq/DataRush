5cc4f9bddfa86e4af888bcc5979bd3df1df87684
ITQ <itq.dev@ya.ru>
2025-03-01 16:54:25 +0300

    <type>(scope): <description>
    [body]
    
    [footer(s)]

--- services/backend/apps/task/models.py
+++ services/backend/apps/task/models.py
@@ -20,7 +20,7 @@ class CompetitionTask(BaseModel):
     competition = models.ForeignKey(Competition, on_delete=models.CASCADE)
     title = models.CharField(verbose_name="заголовок", max_length=50)
     description = models.TextField(verbose_name="описание", max_length=300)
-    max_attemps = models.PositiveSmallIntegerField()
+    max_attemps = models.PositiveSmallIntegerField(default=0)
     type = models.CharField(
         choices=CompetitionTaskType, max_length=8, verbose_name="тип проверки"
     )
@@ -50,14 +50,6 @@ class CompetitionTask(BaseModel):
         blank=True,
         null=True,
         verbose_name="критерии",
-        default=lambda: [
-            {
-                "name": "CHANGE ME",
-                "slug": "CHANGE ME",
-                "max_value": 0,
-                "min_value": 0,
-            }
-        ],
     )
 
     def clean(self):
