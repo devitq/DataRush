bef7337d1c0c361de66d6d51c4ef80312ac3b513
Timur <jungle8888@mail.ru>
2025-03-02 13:53:57 +0300

    remove reviews admin

--- services/backend/apps/review/admin.py
+++ services/backend/apps/review/admin.py
@@ -7,11 +7,3 @@ from apps.review.models import Review, Reviewer
 class ReviewersAdmin(admin.ModelAdmin):
     list_display = ("name", "surname",)
     search_fields = ("name", "surname",)
-
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ("id", "reviewer", "submission",)
-    search_fields = ("id", "reviewer__id", "reviewer__name", "reviewer__surname",
-                     "submission__id", "submission__content")
-    list_filter = ("submission__plagiarism_checked", "submission__status",)
