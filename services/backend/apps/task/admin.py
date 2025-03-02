26a7b9d43bdfc7e86bd20da58f23b1c36b930f11
Timur <jungle8888@mail.ru>
2025-03-02 12:57:22 +0300

    add submissions admin

--- services/backend/apps/task/admin.py
+++ services/backend/apps/task/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from apps.task.models import CompetitionTask, CompetitionTaskAttachment
+from apps.task.models import CompetitionTask, CompetitionTaskAttachment, \
+    CompetitionTaskSubmission
 
 
 class CompletionAttachmentInline(admin.StackedInline):
@@ -11,6 +12,20 @@ class CompletionAttachmentInline(admin.StackedInline):
 @admin.register(CompetitionTask)
 class CompetitionTaskAdmin(admin.ModelAdmin):
     list_display = ("title", "type", "points")
+
+
+@admin.register(CompetitionTaskSubmission)
+class CompetitionTaskSubmissionAdmin(admin.ModelAdmin):
+    list_display = ("task", "user", "status",)
+    search_fields = ("task__id", "task__title", "user__username", "user__email")
+    filter = ("plagiarism_checked",)
+    ordering = "-timestamp"
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 
 class CompetitionTaskInline(admin.StackedInline):
