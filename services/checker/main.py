87b73e804b4e828a7fdf4aa8214864183332c417
ITQ <itq.dev@ya.ru>
2025-03-03 22:41:12 +0300

    <type>(scope): <description>
    [body]
    
    [footer(s)]

--- services/checker/main.py
+++ services/checker/main.py
@@ -279,7 +279,7 @@ async def execute_code(request: ExecutionRequest) -> ExecutionResponse:
             result_hash=result_hash,
             error=error[:5000],
         )
-        print(response)
+        print(response.model_dump_json())
         return response
 
 
