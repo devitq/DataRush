8a27c84d17a57a34663850089527d97c6b5a8ad3
rngsurrounded <kircore970@gmail.com>
2025-03-03 20:38:36 +0900

    fix test

--- services/frontend/src/pages/CompetitionSession/components/CompetitionHeader/index.tsx
+++ services/frontend/src/pages/CompetitionSession/components/CompetitionHeader/index.tsx
@@ -21,11 +21,10 @@ const CompetitionHeader: React.FC<CompetitionHeaderProps> = ({
 }) => {
   const navigate = useNavigate();
 
-  const handleTaskSelect = (event: React.MouseEvent, taskId: string) => {
-    event.preventDefault();
+  const handleTaskSelect = (taskId: string) => {
     setAnswer("");
     setSelectedFile(null);
-    console.log("link check");
+    console.log("CLICK TEst")
     
     navigate(`/competition/${competitionId}/tasks/${taskId}`);
   }
@@ -50,17 +49,16 @@ const CompetitionHeader: React.FC<CompetitionHeaderProps> = ({
         
         <div className="flex items-center justify-center gap-4 pb-4 overflow-x-auto no-scrollbar">
           {tasks.map((task) => (
-            <Link
-              key={task.id}
-              to={`/competition/${competitionId}/tasks/${task.id}`}
-              className={`text-[var(--color-task-text-uncleared)] bg-[var(--color-task-uncleared)]
-                rounded-lg px-3 py-1.5 font-medium text-sm font-hse-sans cursor-pointer 
-                transition-all hover:brightness-95 flex-shrink-0
-                `}
-              onClick={(e) => handleTaskSelect(e, task.id)}
-            >
-              {task.in_competition_position}
-            </Link>
+          <button
+            key={task.id}
+            className={`text-[var(--color-task-text-uncleared)] bg-[var(--color-task-uncleared)]
+              rounded-lg px-3 py-1.5 font-medium text-sm font-hse-sans cursor-pointer 
+              transition-all hover:brightness-95 flex-shrink-0
+              `}
+            onClick={() => handleTaskSelect(task.id)}
+          >
+            {task.in_competition_position}
+          </button>
           ))}
         </div>
       </div>
